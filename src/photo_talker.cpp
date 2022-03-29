81e0685bee5e30fe6141db708377998d1f486958
Nikolay <41687334+nikrus333@users.noreply.github.com>
2022-03-29 13:12:56 +0300

    add param realtime

--- src/photo_talker.cpp
+++ src/photo_talker.cpp
@@ -1,55 +1,53 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-
 #include <sstream>
 
 #include "nvcam.h"
 #include <stdlib.h>
 #include <stdio.h>
- #include <string.h>
-#include "time.h"
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
-
 int main(int argc, char **argv)
 {
   
   ros::init(argc, argv, "photo_talker");  
   ros::NodeHandle n;
-  ros::Rate loop_rate(25);
+  ros::Rate loop_rate(5);
 
   unsigned int  retval;
   char fname[255];
-  FILE   *f;
   uint8_t  *data;
   uint32_t size;
-
+  bool realtime;
 
   image_transport::ImageTransport it(n);
-  image_transport::Publisher pub_frame = it.advertise("camera", 1);
+  image_transport::Publisher pub_frame = it.advertise("photo", 1);
      
   sensor_msgs::ImagePtr msgv;
-  NVCamera* cam = new NVCamera;
-  if (cam->CameraConnected()) {
-  while (ros::ok())
-  {
+  n.getParam("realtime", realtime);
+  if (realtime == true) {
+    NVCamera* cam = new NVCamera;
   
-    size = cam->PreviewToMemory((uint8_t**)&data);
-    cv::Mat raw_data(1, size, CV_8UC1, (void*) data);
-    cv::Mat img = imdecode(raw_data, cv::IMREAD_ANYCOLOR);
-    //src = cv::cvCloneImage(data);
-    msgv = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
-    pub_frame.publish(msgv);
-    
-    ros::spinOnce();
-    loop_rate.sleep();
+    if (cam->CameraConnected()) {
+      while (ros::ok())
+      {
+        size = cam->PreviewToMemory((uint8_t**)&data);
+        cv::Mat raw_data(1, size, CV_8UC1, (void*) data);
+        cv::Mat img = imdecode(raw_data, cv::IMREAD_ANYCOLOR);
+        //src = cv::cvCloneImage(data);
+        msgv = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
+        pub_frame.publish(msgv);
+        
+        ros::spinOnce();
+        loop_rate.sleep();
+      }
+    }
+    delete cam;
   }
-
-  }
-  delete cam;
+  ros::shutdown();
   return 0;
 }
